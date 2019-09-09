60b509875c1786a1b880bbdf9287ac5bfb9a2300
mrbrunotte <mrbrunotte@gmail.com>
2019-09-09 17:45:01 +0200

    Add changes to app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World ...again and again'
-
+"""
 if __name__ == '__main__':
     # NEVER HAVE DEBUG=TRUE IN PRODUCTION OR WHEN SUBMITTING!!!
     app.run(debug=True)
-
+"""
 """
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
@@ -21,9 +21,9 @@ if __name__ == '__main__':
 
     app.run(host='localhost', port=port)
 """
-"""
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-"""
+
