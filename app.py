6958502b858a72293642cd96ae4594b0ceec46fe
mrbrunotte <mrbrunotte@gmail.com>
2019-09-11 22:04:05 +0200

    Add addtasks and routes

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task-manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:rOOtUser@myfirstcluster-xucy6.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
 
 mongo = PyMongo(app)
 
@@ -15,6 +16,18 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def hello():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
